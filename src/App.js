1568a26a3a3b6e2ff989ef035b09d6afc91bc795
uptoy <u24to45y@gmail.com>
2020-12-21 14:40:48 +0900

    [Done]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chart from './Chart';
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: '70%' }}>
+        <Chart />
+      </div>
+    </div>
+  );
+};
+
+export default App;
